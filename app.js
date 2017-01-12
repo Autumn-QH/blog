97a9dbd31f358af48afa9ec4b39e76775c33471e
autumnQH <380105611@qq.com>
2017-01-12 19:39:44 +0800

    增加log日志和gitignore

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var flash = require('connect-flash');
 var config = require('config-lite');
 var routes = require('./routes');
 var pkg = require('./package');
+var winston = require('winston');
+var expressWinston = require('express-winston');
 
 var app = express();
 
@@ -53,8 +55,32 @@ app.use(function(req, res, next){
   next();
 });
 
+//正常请求的日志
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/success.log'
+    })]
+}));
+
 //路由
 routes(app);
+
+//错误请求的日志
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log'
+    })]
+}));
 
 //错误处理器
 app.use(function(req, res, next) {
